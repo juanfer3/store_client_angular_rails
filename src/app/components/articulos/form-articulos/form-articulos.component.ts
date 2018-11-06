4e3136cf29cd78e91e77868870d384f365079860
Juanfer <jfvilladiego2@misena.edu.co>
2018-11-06 04:47:03 -0500

    Nested form Articles Angular

--- src/app/components/articulos/form-articulos/form-articulos.component.ts
+++ src/app/components/articulos/form-articulos/form-articulos.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 import { FormBuilder, FormGroup, FormArray, Validators, FormControl } from '@angular/forms';
 
+import { Categoria } from '../../../Models/categoria';
+import { CategoriasService } from "../../../services/categorias.service";
+
+
 @Component({
   selector: 'app-form-articulos',
   templateUrl: './form-articulos.component.html',
@@ -9,15 +13,29 @@ import { FormBuilder, FormGroup, FormArray, Validators, FormControl } from '@ang
 })
 export class FormArticulosComponent implements OnInit {
 
+  public categorias: Categoria[];
+
   nestedForm: FormGroup;
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private categoriasService: CategoriasService
+    ) { }
 
 
   ngOnInit() {
     this.nestedForm = this.fb.group({
+      categoria_id: [null, [Validators.required]],
       nombre: [null, [Validators.required]],
+
       imagenes: this.fb.array([this.addImagenGroup()])
-    })
+    });
+
+    this.categoriasService.getCategorias().subscribe(
+      data => {
+        this.categorias = data;
+        console.log(this.categorias);
+      }
+    );
 
   }
 
